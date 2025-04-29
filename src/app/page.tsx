0393a4191c9c7b4787a4131452a996884c8bb58b
roifjcf <roifjcf@gmail.com>
2025-04-30 05:26:46 +1000

    setup & first project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,28 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+
+const nav = [
+  {name: "GreatFrontEnd", url: "https://www.greatfrontend.com/projects/u/roifjcf"},
+  {name: "Github", url: "https://github.com/roifjcf"}
+];
+
+const starter = [
+  "/testimonial-card"
+];
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  <main>
+    <h1>greatfrontend-projects</h1>
+    {/* <p>by roifjcf</p> */}
+    <ul>
+      {nav.map((url, i)=>
+      <li key={i}><Link href={url["url"]} target="_blank">{url["name"]+" ↗"}</Link></li>)}
+    </ul>
+    <h2>Starter</h2>
+    <ul>
+      {starter.map((url, i)=>
+      <li key={i}><Link href={url} target="_blank">{url.substring(1)+" ↗"}</Link></li>)}
+    </ul>
+  </main>
   );
 }
---
+++ src/app/testimonial-card/page.tsx
@@ -0,0 +1,27 @@
+import "./styles.css";
+
+const info = {
+  name: "Sarah Dole",
+  handle: "sarahdole",
+  message: "I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!",
+  img: "/testimonial-card/profile-thumbnail.png",
+};
+
+export default function Page() {
+  return (
+  <main>
+    <div className="testimonial-card-container">
+      <div className="testimonial-card-container-top">
+        <img className="icon" src={info.img} alt="profile-thumbnail" />
+        <div className="testimonial-card-container-top-text">
+          <p className="name">{info.name}</p>
+          <p className="handle">{"@"+info.handle}</p>
+        </div>
+      </div>
+      <p>
+        {info.message}
+      </p>
+    </div>
+  </main>
+  );
+}
